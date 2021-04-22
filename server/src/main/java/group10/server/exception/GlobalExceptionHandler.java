cadb392237c7a786d64df08a538052efe4952d7e
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-04-22 19:31:53 +0300

    enable global exception handler

---
+++ server/src/main/java/group10/server/exception/GlobalExceptionHandler.java
@@ -0,0 +1,64 @@
+package group10.server.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(ArithmeticException.class)
+    public ResponseEntity<?> handleArithmeticException(HttpServletRequest request, ArithmeticException t) {
+        LOGGER.error(t.getMessage(), t);
+        Map<String, String> map = new HashMap<>();
+        map.put("error", "Arithmetic error occurred");
+        return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(HttpServletRequest request, IllegalArgumentException t) {
+        LOGGER.error(t.getMessage(), t);
+        Map<String, String> map = new HashMap<>();
+        map.put("error", t.getMessage());
+        return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleIMethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException t) {
+        LOGGER.error(t.getMessage(), t);
+        BindingResult result = t.getBindingResult();
+        final List<FieldError> fieldErrors = result.getFieldErrors();
+        Map<String, String> map = new HashMap<>();
+        if (fieldErrors.isEmpty()) {
+            map.put("error", "Exception occurred");
+        } else {
+            for (FieldError f : fieldErrors) {
+                map.put("error", f.getDefaultMessage());
+            }
+        }
+        return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<?> handleRuntimeException(HttpServletRequest request, Throwable t) {
+        LOGGER.error(t.getMessage(), t);
+        Map<String, String> map = new HashMap<>();
+        map.put("error", "Runtime error occurred");
+        return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
