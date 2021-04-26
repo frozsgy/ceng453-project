5364656b7a9455dca20ab7a73e982366c7edc026
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-04-27 01:29:09 +0300

    jwt tokens implemented

--- server/src/main/java/group10/server/security/JWTConstants.java
+++ server/src/main/java/group10/server/security/JWTConstants.java
@@ -1,2 +1,9 @@
-package group10.server.security;public class JWTConstants {
+package group10.server.security;
+
+public class JWTConstants {
+    protected final static int EXPIRES_IN = 600_000_000; // in ms
+    protected final static String SECRET_KEY = "CENG453rules!.";
+    protected final static String AUTH_HEADER = "Authorization";
+    protected final static String TOKEN_PREFIX = "Bearer ";
+    protected final static String DECODE_ON_SIGN = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
 }
