0ec70bcbfafb739ff7c5bd17eb73f5f7d7a38237
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-04-26 23:39:16 +0300

    Web security enabled

--- server/src/main/java/group10/server/ServerApplication.java
+++ server/src/main/java/group10/server/ServerApplication.java
@@ -3,7 +3,13 @@ package group10.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -16,4 +22,16 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 
+    @EnableWebSecurity
+    @Configuration
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/api/user/login").permitAll()
+                    .antMatchers(HttpMethod.POST, "/api/user/register").permitAll()
+                    .anyRequest().authenticated();
+        }
+    }
 }
