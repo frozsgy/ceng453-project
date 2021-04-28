bd3f7b63601f3cc843a6756dc5480a46084f2eb9
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-04-29 00:08:26 +0300

    WebSecuritConfig migrated to security package, constants updated accordingly

--- server/src/main/java/group10/server/ServerApplication.java
+++ server/src/main/java/group10/server/ServerApplication.java
@@ -1,17 +1,9 @@
 package group10.server;
 
-import group10.server.security.JWTAuthorizationFilter;
-import group10.server.security.JWTConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -19,20 +11,6 @@ public class ServerApplication {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @EnableWebSecurity
-    @Configuration
-    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable()
-                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-                    .authorizeRequests()
-                    .antMatchers(HttpMethod.POST, JWTConstants.LOGIN_PATH).permitAll()
-                    .antMatchers(HttpMethod.POST, JWTConstants.REGISTER_PATH).permitAll()
-                    .anyRequest().authenticated();
-        }
     }
 
     public static void main(String[] args) {
--- server/src/main/java/group10/server/security/JWTConstants.java
+++ server/src/main/java/group10/server/security/JWTConstants.java
@@ -1,10 +1,10 @@
 package group10.server.security;
 
 public class JWTConstants {
-    protected final static int EXPIRES_IN = 600_000_000; // in ms
+    protected final static int EXPIRES_IN = 60_000; // in ms
     protected final static String SECRET_KEY = "CENG453rules!.";
     protected final static String AUTH_HEADER = "Authorization";
     protected final static String TOKEN_PREFIX = "Bearer ";
-    public final static String LOGIN_PATH = "/api/user/login";
-    public final static String REGISTER_PATH = "/api/user/register";
+    protected final static String LOGIN_PATH = "/api/user/login";
+    protected final static String REGISTER_PATH = "/api/user/register";
 }
