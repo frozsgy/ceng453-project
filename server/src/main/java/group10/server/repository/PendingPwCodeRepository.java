9d7790f1c585ddc8b77caa5a996690cd574e6459
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-04-30 00:19:50 +0300

    added pending pw repository

--- server/src/main/java/group10/server/repository/PendingPwCodeRepository.java
+++ server/src/main/java/group10/server/repository/PendingPwCodeRepository.java
@@ -1,2 +1,14 @@
-package group10.server.repository;public interface PendingPwCodeRepository {
+package group10.server.repository;
+
+import group10.server.entity.PendingPwCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PendingPwCodeRepository extends JpaRepository<PendingPwCode, Long> {
+
+    Optional<PendingPwCode> findById(long id);
+
 }
