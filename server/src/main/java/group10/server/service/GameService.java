2b9ea20b6276f5e91bd53eb8226dc728ef98fd3b
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-04-29 22:55:37 +0300

    create new game method

--- server/src/main/java/group10/server/service/GameService.java
+++ server/src/main/java/group10/server/service/GameService.java
@@ -1,7 +1,9 @@
 package group10.server.service;
 
 import group10.server.entity.Game;
+import group10.server.entity.Match;
 import group10.server.entity.Player;
+import group10.server.enums.GameTypes;
 import group10.server.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,10 +18,15 @@ public class GameService {
         this.gameRepository = gameRepository;
     }
 
-    public Game newGame(Player player) {
-        // TODO
-        System.out.println("new game request");
-        return null;
+    public Match newGame(Player player) {
+        // TODO -- check
+        Game game = new Game();
+        game.setGameType(GameTypes.AI);
+        Match match = new Match();
+        match.setLevel(1);
+        match.setGame(game);
+        match.setPlayer(player);
+        return match;
     }
 
     public Game finishLevel(Player player, Game game) {
