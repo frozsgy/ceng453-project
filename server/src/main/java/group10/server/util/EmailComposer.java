2da0a986410f16599cf54afd6f8f13bb40050dee
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-04-30 00:46:51 +0300

    implemented utility to compose emails

--- server/src/main/java/group10/server/util/EmailComposer.java
+++ server/src/main/java/group10/server/util/EmailComposer.java
@@ -1,2 +1,17 @@
-package group10.server.util;public class EmailComposer {
+package group10.server.util;
+
+import org.springframework.mail.SimpleMailMessage;
+
+public class EmailComposer {
+
+    private static final String SUBJECT = "Your Password Code Request";
+    private static final String BODY = "Your code: ";
+
+    public static SimpleMailMessage composeMail(String email, String code) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(email);
+        msg.setSubject(SUBJECT);
+        msg.setText(BODY + code);
+        return msg;
+    }
 }
