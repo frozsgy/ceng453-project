27aea530996de9f1b064eb3fae9f8f22ffcf2a43
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-04-30 00:12:48 +0300

    util package init, random string generation implemented

--- server/src/main/java/group10/server/util/RandomStringGenerator.java
+++ server/src/main/java/group10/server/util/RandomStringGenerator.java
@@ -1,4 +1,24 @@
 package group10.server.util;
 
+import java.util.Random;
+
 public class RandomStringGenerator {
+
+    private final static int STRING_LENGTH = 10;
+    private final static String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final static String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private final static String NUMBERS = "0123456789";
+    private final static String PUNCTS = "!@#$%^&*()_+=-.,;";
+    private final static String ALL = UPPER + LOWER + NUMBERS + PUNCTS;
+
+    public static String generate() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < STRING_LENGTH; i++) {
+            int index = random.nextInt(ALL.length());
+            char randChar = ALL.charAt(index);
+            sb.append(randChar);
+        }
+        return sb.toString();
+    }
 }
