c34586f283340fd279a12b4024c42c5754c49c8c
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-04-30 00:48:06 +0300

    enable swagger

--- server/src/main/java/group10/server/config/WebSecurityConfig.java
+++ server/src/main/java/group10/server/config/WebSecurityConfig.java
@@ -10,11 +10,27 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST = { //
+            "/h2-console", //
+            "/h2-console/**", //
+            "/v2/api-docs", //
+            "/swagger-resources", //
+            "/swagger-resources/**", //
+            "/configuration/ui", //
+            "/configuration/security", //
+            "/swagger-ui.html", //
+            "/webjars/**", //
+            "/graphiql", //
+            "/api/graphql", //
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .antMatchers(HttpMethod.POST, JWTConstants.LOGIN_PATH).permitAll()
                 .antMatchers(HttpMethod.POST, JWTConstants.REGISTER_PATH).permitAll()
                 .anyRequest().authenticated();
