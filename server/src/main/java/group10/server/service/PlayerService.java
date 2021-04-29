53771afbf913f86a0571433a885429d695129ae1
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-04-29 23:43:41 +0300

    add role to player entity

--- server/src/main/java/group10/server/service/PlayerService.java
+++ server/src/main/java/group10/server/service/PlayerService.java
@@ -1,11 +1,12 @@
 package group10.server.service;
 
+import group10.server.config.JWTUtil;
 import group10.server.entity.Player;
 import group10.server.model.LoginDTO;
 import group10.server.model.PasswordDTO;
 import group10.server.model.PlayerDTO;
 import group10.server.repository.PlayerRepository;
-import group10.server.config.JWTUtil;
+import group10.server.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -15,19 +16,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.Optional;
 
 @Service
 public class PlayerService {
 
     private PlayerRepository playerRepository;
+    private RoleRepository roleRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     private JavaMailSender javaMailSender;
 
     @Autowired
-    public PlayerService(PlayerRepository playerRepository, BCryptPasswordEncoder bCryptPasswordEncoder,
-                         JavaMailSender javaMailSender) {
+    public PlayerService(PlayerRepository playerRepository, RoleRepository roleRepository,
+                         BCryptPasswordEncoder bCryptPasswordEncoder, JavaMailSender javaMailSender) {
         this.playerRepository = playerRepository;
+        this.roleRepository = roleRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.javaMailSender = javaMailSender;
     }
@@ -37,6 +41,7 @@ public class PlayerService {
         player.setUsername(dto.getUsername());
         player.setEmail(dto.getEmail());
         player.setPassword(this.bCryptPasswordEncoder.encode(dto.getPassword()));
+        player.setRoles(Arrays.asList(roleRepository.findByName("ROLE_USER")));
         long userId = -1;
         try {
             Player saved = playerRepository.save(player);
