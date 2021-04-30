70eaa4e3602a231e586fac96594035ffac09f8da
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-04-30 12:03:27 +0300

    create sql exception handler

--- server/src/main/java/group10/server/exception/GlobalExceptionHandler.java
+++ server/src/main/java/group10/server/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package group10.server.exception;
 
+import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -52,6 +55,13 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleSQLException(HttpServletRequest request, DataIntegrityViolationException t) {
+        LOGGER.error(t.getMessage(), t);
+        Map<String, String> map = new HashMap<>();
+        map.put("error", "Key violation");
+        return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 
     @ExceptionHandler(Throwable.class)
