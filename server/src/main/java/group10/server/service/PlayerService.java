531980489402d3c03b0c6cb1773a438fc9593763
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-05-01 00:43:50 +0300

    javadoc for service done

--- server/src/main/java/group10/server/service/PlayerService.java
+++ server/src/main/java/group10/server/service/PlayerService.java
@@ -26,15 +26,45 @@ import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.Optional;
 
+/**
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ * Service class that is responsible of Player.
+ */
 @Service
 public class PlayerService {
 
+    /**
+     * Auto-wired player repository. Responsible for interacting with Player table.
+     */
     private PlayerRepository playerRepository;
+    /**
+     * Auto-wired role repostiory. Responsible for interacting with Role table.
+     */
     private RoleRepository roleRepository;
+    /**
+     * Auto-wired password encoder.
+     * Generates salted and hashed password.
+     * Also, verifies password for login.
+     */
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    /**
+     * Auto-wired mail sender.
+     * Sends e-mail to given address.
+     */
     private JavaMailSender javaMailSender;
+    /**
+     * Auto-wired repository that is responsible for interacting with pending_pw_code table.
+     */
     private PendingPwCodeRepository pendingPwCodeRepository;
 
+    /**
+     * Constructor for player service.
+     * @param playerRepository Autowired player repository
+     * @param roleRepository Autowired role repository.
+     * @param bCryptPasswordEncoder Autowired password encoder.
+     * @param javaMailSender Autowired mail sender.
+     * @param pendingPwCodeRepository Autowired pending pw code repository.
+     */
     @Autowired
     public PlayerService(PlayerRepository playerRepository, RoleRepository roleRepository,
                          BCryptPasswordEncoder bCryptPasswordEncoder, JavaMailSender javaMailSender,
@@ -46,6 +76,14 @@ public class PlayerService {
         this.pendingPwCodeRepository = pendingPwCodeRepository;
     }
 
+    /**
+     * Registers (adds) a user to Player table.
+     * @param dto DTO object that registered player is created from.
+     * @return Player
+     * @throws IllegalArgumentException if any of the dto fields is invalid.
+     * @see Player
+     * @see PlayerDTO
+     */
     public Player register(PlayerDTO dto) throws IllegalArgumentException {
         Player player = new Player();
         player.setUsername(dto.getUsername());
@@ -55,6 +93,13 @@ public class PlayerService {
         return playerRepository.save(player);
     }
 
+    /**
+     *
+     * Logs in a registered player.
+     * @param loginData DTO object that contains username and password.
+     * @return Token
+     * @throws IllegalArgumentException if username and password do not match.
+     */
     public String login(LoginDTO loginData) throws IllegalArgumentException {
         Optional<Player> optUser = playerRepository.findByUsername(loginData.getUsername());
         if (optUser.isPresent()) {
@@ -66,6 +111,12 @@ public class PlayerService {
         throw new IllegalArgumentException("Wrong username/password");
     }
 
+    /**
+     * Given an email address, if it is registered, creates a password reset code, mails it and
+     * saves it to pending_pw_code table.
+     * @param emailJSON JSON object that contains the password that the mail is going to be sent.
+     * @return true if user is found and mail is sent successfully, false otherwise.
+     */
     public boolean requestPwCode(JSONObject emailJSON) {
         try{
             String email = emailJSON.getString("email");
@@ -92,6 +143,12 @@ public class PlayerService {
         }
     }
 
+    /**
+     * Given username, password reset code and a new password, if credentials are valid, sets the user's password
+     * to new password.
+     * @param passwordResetDTO DTO that contains, username, new password and password reset code.
+     * @return true if password is updated successfully, false otherwise.
+     */
     @Transactional
     public boolean updatePassword(PasswordResetDTO passwordResetDTO) {
         Optional<PendingPwCode> optionalPending = pendingPwCodeRepository.findByCode(passwordResetDTO.getResetCode());
@@ -110,6 +167,10 @@ public class PlayerService {
         }
     }
 
+    /**
+     * Given token, gets the Player
+     * @return Player
+     */
     public Player getLoggedInPlayer() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String username = (String) auth.getPrincipal();
