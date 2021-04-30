d69937480830be1101cea972350938f1fa7d0359
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-04-30 15:19:37 +0300

    add setters

--- server/src/main/java/group10/server/model/LoginDTO.java
+++ server/src/main/java/group10/server/model/LoginDTO.java
@@ -15,4 +15,12 @@ public class LoginDTO {
     public String getPassword() {
         return password;
     }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
