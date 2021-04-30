f20b32883b3e0c56ff1c9c14ac5ac3a565a1c836
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-05-01 01:40:06 +0300

    update global exception handler

--- server/src/main/java/group10/server/exception/GlobalExceptionHandler.java
+++ server/src/main/java/group10/server/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package group10.server.exception;
 
-import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -13,35 +12,60 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+/**
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ * Global Exception Handler that is responsible of handling various types of exceptions throughout the application.
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * Static Logger instance for logging purposes.
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    /**
+     * Handler for ArihmeticException
+     * @param request Incoming HttpServletRequest
+     * @param t Exception object
+     * @return Internal Server Error
+     */
     @ExceptionHandler(ArithmeticException.class)
     public ResponseEntity<?> handleArithmeticException(HttpServletRequest request, ArithmeticException t) {
-        LOGGER.error(t.getMessage(), t);
+        LOGGER.info("Caught exception: " + t.getMessage());
         Map<String, String> map = new HashMap<>();
         map.put("error", "Arithmetic error occurred");
         return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Handler for IllegalArgumentException
+     * @param request Incoming HttpServletRequest
+     * @param t Exception object
+     * @return Internal Server Error
+     */
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> handleIllegalArgumentException(HttpServletRequest request, IllegalArgumentException t) {
-        LOGGER.error(t.getMessage(), t);
+        LOGGER.info("Caught exception: " + t.getMessage());
         Map<String, String> map = new HashMap<>();
         map.put("error", t.getMessage());
         return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Handler for MethodArgumentNotValidException
+     * @param request Incoming HttpServletRequest
+     * @param t Exception object
+     * @return Internal Server Error
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleIMethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException t) {
-        LOGGER.error(t.getMessage(), t);
+        LOGGER.info("Caught exception: " + t.getMessage());
         BindingResult result = t.getBindingResult();
         final List<FieldError> fieldErrors = result.getFieldErrors();
         Map<String, String> map = new HashMap<>();
@@ -55,18 +79,29 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Handler for DataIntegrityViolationException
+     * @param request Incoming HttpServletRequest
+     * @param t Exception object
+     * @return Internal Server Error
+     */
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<?> handleSQLException(HttpServletRequest request, DataIntegrityViolationException t) {
-        LOGGER.error(t.getMessage(), t);
+        LOGGER.info("Caught exception: " + t.getMessage());
         Map<String, String> map = new HashMap<>();
         map.put("error", "Key violation");
         return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-
+    /**
+     * Handler for Throwable
+     * @param request Incoming HttpServletRequest
+     * @param t Exception object
+     * @return Internal Server Error
+     */
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<?> handleRuntimeException(HttpServletRequest request, Throwable t) {
-        LOGGER.error(t.getMessage(), t);
+        LOGGER.info("Caught exception: " + t.getMessage());
         Map<String, String> map = new HashMap<>();
         map.put("error", "Runtime error occurred");
         return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
