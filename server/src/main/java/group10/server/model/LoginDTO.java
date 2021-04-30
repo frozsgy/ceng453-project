3d447f5ab55b652db8e89e7c16520f148f0110e4
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-05-01 01:51:01 +0300

    model javadoc done

--- server/src/main/java/group10/server/model/LoginDTO.java
+++ server/src/main/java/group10/server/model/LoginDTO.java
@@ -2,24 +2,51 @@ package group10.server.model;
 
 import javax.validation.constraints.NotBlank;
 
+/**
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ * Data transfer object that is received from client.
+ * Carries log in information, which are username and password.
+ */
 public class LoginDTO {
+    /**
+     * Not blank username
+     */
     @NotBlank
     private String username;
+    /**
+     * Not blank password.
+     */
     @NotBlank
     private String password;
 
+    /**
+     * Gets the username
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Gets the password.
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the username
+     * @param username username to be set
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Sets the password
+     * @param password password to be set
+     */
     public void setPassword(String password) {
         this.password = password;
     }
--- server/src/main/java/group10/server/model/MatchDTO.java
+++ server/src/main/java/group10/server/model/MatchDTO.java
@@ -2,28 +2,52 @@ package group10.server.model;
 
 import org.hibernate.validator.constraints.Range;
 
-
+/**
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ * Data transfer object that is received from client.
+ * Carries level information, which are score and game
+ */
 public class MatchDTO {
 
+    /**
+     * Non empty score of the level.
+     */
     @Range(min = 0, message= "Score may not be empty")
     private int score;
-
+    /**
+     * Non empty game id of this level belongs to.
+     */
     @Range(min = 0, message= "Game may not be empty")
     private long game;
 
-
+    /**
+     * Gets the score
+     * @return score
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Sets the score
+     * @param score score to be set
+     */
     public void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     * Gets the game
+     * @return game of this level
+     */
     public long getGame() {
         return game;
     }
 
+    /**
+     * Sets the game of this level.
+     * @param game Game to be set
+     */
     public void setGame(long game) {
         this.game = game;
     }
