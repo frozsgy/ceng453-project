92fded1160f0055916a9456c93da319787e9d7b9
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-04-30 19:36:07 +0300

    repo test converted to service test, db persistency disabled for test

--- server/src/test/java/group10/server/service/PlayerServiceTests.java
+++ server/src/test/java/group10/server/service/PlayerServiceTests.java
@@ -1,40 +1,41 @@
 package group10.server.service;
 
 
-import group10.server.entity.Player;
 import group10.server.model.PlayerDTO;
-import group10.server.repository.PlayerRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
-@DataJpaTest
+@SpringBootTest
 public class PlayerServiceTests {
 
-    private final String testUsername = "testUser";
+    private final String testUsername = "testUserX";
     private final String testPassword = "testPassword";
-    private final String testEmail = "test@gmail.com";
+    private final String testEmail = "testXq@gmail.com";
     private final String empty = "";
     private final String wrongCode = "adsgtaas";
 
 
     @Autowired
-    private PlayerRepository playerRepository;
+    private PlayerService playerService;
 
     @Test
     @DisplayName("Service - User Register")
+    @Transactional
     @Order(1)
     void registerTest() {
-        Player player = new Player();
+        PlayerDTO player = new PlayerDTO();
         player.setEmail(testEmail);
         player.setPassword(testPassword);
         player.setUsername(testUsername);
-        assertThat(playerRepository.save(player), notNullValue());
+        assertThat(playerService.register(player), notNullValue());
     }
 
 }
