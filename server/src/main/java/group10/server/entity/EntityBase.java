2cf8f605a5a0523bc38b3a8a6804c8084458bae7
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-05-01 01:51:10 +0300

    add javadoc to entity base

--- server/src/main/java/group10/server/entity/EntityBase.java
+++ server/src/main/java/group10/server/entity/EntityBase.java
@@ -4,26 +4,51 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ * Entity Base that is used by all entities, and contains common fields.
+ */
 @MappedSuperclass
 public class EntityBase implements Serializable {
 
+    /**
+     * Primary Key that is automatically incremented by every insert.
+     */
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    /**
+     * Create date for the entry.
+     */
     @Column(name = "create_date")
     private Date createDate;
 
+    /**
+     * Update date for the entry.
+     */
     @Column(name = "update_date")
     private Date updateDate;
 
+    /**
+     * Boolean field to state if the entry is soft-deleted or not.
+     */
     @Column(name = "active")
     private boolean active;
 
+    /**
+     * Field that stores the operation type for the most recent operation that has happened.
+     * Can be either save, update or delete.
+     */
     @Column(name = "operation_type")
     private String operationType;
 
+    /**
+     * Method that gets invoked before insertion.
+     * Sets the operation type to save, marks as active,
+     * and fills in the create and update date values.
+     */
     @PrePersist
     public void onPrePersist() {
         this.setOperationType("save");
@@ -32,12 +57,21 @@ public class EntityBase implements Serializable {
         this.setUpdateDate(new Date());
     }
 
+    /**
+     * Method that gets invoked on update.
+     * Sets the operation type to update, and updates the update date value.
+     */
     @PreUpdate
     public void onPreUpdate() {
         this.setOperationType("update");
         this.setUpdateDate(new Date());
     }
 
+    /**
+     * Method that gets invoked before deletion.
+     * Sets the operation type to delete, marks as inactive,
+     * and updates the update date value.
+     */
     @PreRemove
     public void onPreRemove() {
         this.setOperationType("delete");
@@ -45,43 +79,92 @@ public class EntityBase implements Serializable {
         this.setActive(false);
     }
 
-
+    /**
+     * Gets id.
+     *
+     * @return id
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Gets create date.
+     *
+     * @return createDate
+     */
     public Date getCreateDate() {
         return createDate;
     }
 
+    /**
+     * Sets create date.
+     *
+     * @param createDate
+     */
     public void setCreateDate(Date createDate) {
         this.createDate = createDate;
     }
 
+    /**
+     * Gets update date.
+     *
+     * @return updateDate
+     */
     public Date getUpdateDate() {
         return updateDate;
     }
 
+    /**
+     * Sets update date.
+     *
+     * @param updateDate
+     */
     public void setUpdateDate(Date updateDate) {
         this.updateDate = updateDate;
     }
 
+    /**
+     * Gets active status.
+     *
+     * @return active
+     */
     public boolean isActive() {
         return active;
     }
 
+    /**
+     * Sets active status.
+     *
+     * @param active
+     */
     public void setActive(boolean active) {
         this.active = active;
     }
 
+    /**
+     * Gets operation type.
+     *
+     * @return operationType
+     */
     public String getOperationType() {
         return operationType;
     }
 
+    /**
+     * Sets operation type.
+     *
+     * @param operationType
+     */
     public void setOperationType(String operationType) {
         this.operationType = operationType;
     }
