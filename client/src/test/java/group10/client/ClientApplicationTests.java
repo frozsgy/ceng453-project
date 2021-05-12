a67e4c44cf57a6938e9f93ff20fcbe697ce2a7c1
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-05-12 14:40:54 +0300

    init client project

---
+++ client/src/test/java/group10/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package group10.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
