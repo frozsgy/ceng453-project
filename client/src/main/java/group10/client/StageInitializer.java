a90d57c947ae39945ac92dbced1b50c5ceb631a4
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-05-12 15:19:51 +0300

    create main window

--- client/src/main/java/group10/client/StageInitializer.java
+++ client/src/main/java/group10/client/StageInitializer.java
@@ -1,14 +1,47 @@
 package group10.client;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class StageInitializer implements ApplicationListener<UiApplication.StageReadyEvent> {
+    @Value("classpath:/ui.fxml")
+    private Resource uiResuorce;
+    @Value("${spring.application.ui.width}")
+    private int windowWidth;
+    @Value("${spring.application.ui.height}")
+    private int windowHeight;
+    private String applicationTitle;
+    private ApplicationContext applicationContext;
+
+    public StageInitializer(@Value("${spring.application.ui.title}") String applicationTitle,
+                            ApplicationContext applicationContext) {
+        this.applicationTitle = applicationTitle;
+        this.applicationContext = applicationContext;
+    }
 
     @Override
     public void onApplicationEvent(UiApplication.StageReadyEvent stageReadyEvent) {
-        Stage stage = stageReadyEvent.getStage();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(uiResuorce.getURL());
+            Parent parent = fxmlLoader.load();
+
+            Stage stage = stageReadyEvent.getStage();
+            stage.setScene(new Scene(parent, windowWidth, windowHeight));
+            stage.setTitle(applicationTitle);
+            stage.show();
+
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
---
+++ client/src/main/java/group10/client/controller/UiController.java
@@ -0,0 +1,12 @@
+package group10.client.controller;
+
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class UiController  {
+
+}
