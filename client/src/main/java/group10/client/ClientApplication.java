c39dda962a6cf33738b8d01272d80b5d50069e49
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-05-12 15:25:08 +0300

    optimize imports

--- client/src/main/java/group10/client/ClientApplication.java
+++ client/src/main/java/group10/client/ClientApplication.java
@@ -1,14 +1,13 @@
 package group10.client;
 
 import javafx.application.Application;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientApplication {
 
-	public static void main(String[] args) {
-		Application.launch(UiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Application.launch(UiApplication.class, args);
+    }
 
 }
