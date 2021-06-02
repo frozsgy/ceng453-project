6d660fdc9e12a0e061b0e5d810eba529861d7c41
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-06-02 13:34:00 +0300

    add scoring logic

--- client/src/main/java/group10/client/controller/GameController.java
+++ client/src/main/java/group10/client/controller/GameController.java
@@ -1,6 +1,7 @@
 package group10.client.controller;
 
 import group10.client.enums.Cards;
+import group10.client.enums.PlayerEnum;
 import group10.client.enums.Suits;
 import group10.client.model.Card;
 import javafx.fxml.FXML;
@@ -55,10 +56,14 @@ public class GameController implements Initializable {
     private Stack<Card> allCards;
     private List<Rectangle> currentCards;
     private Map<Rectangle, Card> cardMappings;
-    private int lastWinner = 0;
+    private PlayerEnum lastWinner = PlayerEnum.NULL;
+    private Map<PlayerEnum, Integer> scores;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.scores = new HashMap<>();
+        this.scores.put(PlayerEnum.ONE, 0);
+        this.scores.put(PlayerEnum.TWO, 0);
         this.cardMappings = new HashMap<>();
         Image img = new Image("/static/card_full.png");
         this.opponentCard1.setFill(new ImagePattern(img));
@@ -168,14 +173,22 @@ public class GameController implements Initializable {
         }
     }
 
-    private boolean checkIfMatch(Card candidateCard) {
+    private boolean checkIfMatch(Card candidateCard, PlayerEnum player) {
         Card topCard = this.middle.peek();
+        Integer currentScore = this.scores.get(player);
         if (this.middle.size() == 1 && candidateCard.equals(topCard)) {
+            // TODO -- what if pişti with two of clubs and another two? a better implementation needed here
             if (candidateCard.getCard() == Cards.JACK) {
                 // TODO -- double pişti :: 20 points
+                this.scores.replace(player, currentScore + 20);
+            } else if (candidateCard.getCard() == Cards.ACE) {
+                // TODO -- pişti with ACE's? --
+                this.scores.replace(player, currentScore + 12);
             } else {
                 // TODO -- pişti :: 10 points
+                this.scores.replace(player, currentScore + 10);
             }
+            this.middle.empty();
             return true;
         } else if (candidateCard.equals(topCard) || candidateCard.getCard() == Cards.JACK) {
             // TODO -- empty stack and calculate scores, and save scores
@@ -191,9 +204,22 @@ public class GameController implements Initializable {
         // each ace -- 1
         // two of clubs -- 2
         // ten of diamonds -- 3
-        // most cards -- 3 :: equality -> no team
+        // TODO -- most cards -- 3 :: equality -> no team
         // pisti -- 10
         // double pisti -- 20
-        return 0;
+        int score = 0;
+        while (!this.middle.isEmpty()) {
+            Card pop = this.middle.pop();
+            Cards card = pop.getCard();
+            Suits suit = pop.getSuit();
+            if (card == Cards.JACK || card == Cards.ACE) {
+                score++;
+            } else if (card == Cards.TWO && suit == Suits.CLUB) {
+                score += 2;
+            } else if (card == Cards.TEN && suit == Suits.DIAMOND) {
+                score += 3;
+            }
+        }
+        return score;
     }
 }
