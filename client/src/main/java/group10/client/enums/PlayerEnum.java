aec31aff357494b587d1e7db16f81fb4a81b37d9
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-06-02 13:33:54 +0300

    add player enum

---
+++ client/src/main/java/group10/client/enums/PlayerEnum.java
@@ -0,0 +1,13 @@
+package group10.client.enums;
+
+public enum PlayerEnum {
+    NULL(0),
+    ONE(1),
+    TWO(1);
+
+    private int id;
+
+    PlayerEnum(int id) {
+        this.id = id;
+    }
+}
