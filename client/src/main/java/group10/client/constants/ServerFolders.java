121333858e787cdd35897809b435c02f4f11b3fe
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-06-02 03:46:37 +0300

    Fix 404 on game

--- client/src/main/java/group10/client/constants/ServerFolders.java
+++ client/src/main/java/group10/client/constants/ServerFolders.java
@@ -2,7 +2,7 @@ package group10.client.constants;
 
 public class ServerFolders {
     private final static String USER = "/user/";
-    private final static String GAME = "/game";
+    private final static String GAME = "/game/";
     public final static String LOGIN_PATH = USER + "login";
     public final static String REGISTER_PATH = USER + "register";
     public final static String REQUEST_CODE_PATH = USER + "requestPwCode";
