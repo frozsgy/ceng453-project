2f5a92cbf560f86724575986702870e2ef45ecad
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-06-03 19:34:03 +0300

    fix card display problem after accepting bluf

--- client/src/main/java/group10/client/controller/GameController.java
+++ client/src/main/java/group10/client/controller/GameController.java
@@ -354,8 +354,10 @@ public class GameController implements Initializable {
             // bluf was fake.
             System.out.println("Bluf was fake.");
             Rectangle r = GameLogic.getRectangleByCard(this.cardMappings, bluffed); //get the rectangle of closed card.
+            this.midStack.getChildren().remove(r);
             this.setRectangleVisible(r); // make rectangle visible.
             this.drawCardInsideRectangle(r, bluffed); // put text to it.
+            this.midStack.getChildren().add(r.getParent());
             GameLogic.getInstance().addScoreToPlayer(PlayerEnum.ONE, GameConstants.DOUBLE_PISTI); // give points to player.
             this.setPlayerScore(GameLogic.getInstance().getScores().get(PlayerEnum.ONE)); // update player score view.
             GameLogic.getInstance().getMiddle().push(candidate); // put things back to middle.
