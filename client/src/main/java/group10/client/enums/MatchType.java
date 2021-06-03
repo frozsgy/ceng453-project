04f06fca7cb95cf860be01a383050a6c3cb51cd7
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-06-03 19:03:16 +0300

    remove redundant variables from match type enum

--- client/src/main/java/group10/client/enums/MatchType.java
+++ client/src/main/java/group10/client/enums/MatchType.java
@@ -1,16 +1,9 @@
 package group10.client.enums;
 
-import group10.client.constants.GameConstants;
-
 public enum MatchType {
-    NO(0),
-    REGULAR(1),
-    PISTI(GameConstants.PISTI),
-    DOUBLE_PISTI(GameConstants.DOUBLE_PISTI);
+    NO,
+    REGULAR,
+    PISTI,
+    DOUBLE_PISTI;
 
-    private int score;
-
-    MatchType(int score) {
-        this.score = score;
-    }
 }
