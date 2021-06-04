9bd8b32845afa2413717a8e7290a234a181d0c70
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-06-04 21:29:03 +0300

    remove unused

--- client/src/test/java/group10/client/controller/GameControllerTest_JUnit5AssertJ.java
+++ client/src/test/java/group10/client/controller/GameControllerTest_JUnit5AssertJ.java
@@ -16,20 +16,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.core.annotation.Order;
 import org.testfx.api.FxRobot;
-import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @ExtendWith(ApplicationExtension.class)
 class GameControllerTest_JUnit5AssertJ {
@@ -214,6 +209,4 @@ class GameControllerTest_JUnit5AssertJ {
             break;
         }
     }
-
-
 }
