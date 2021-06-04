6a1a64241e3ce99419ccaf0659bc2d415a8ac1c8
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-06-04 14:38:42 +0300

    http javadoc

--- client/src/main/java/group10/client/service/HTTPService.java
+++ client/src/main/java/group10/client/service/HTTPService.java
@@ -21,19 +21,47 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
+/**
+ *  @author Alperen Caykus, Mustafa Ozan Alpay
+ *  Singleton Service class that is responsible of sending
+ *  HTTP requests to server. Acts as a wrapper class.
+ */
 @Service
 public class HTTPService {
 
+    /**
+     * Constant logger instance to print messages to console.
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(HTTPService.class);
 
-    @Value("${spring.application.api-address}")
-    private String apiAddress;
+    /**
+     * Rest template instance that sends requests to server.
+     */
     private RestTemplate restTemplate;
+    /**
+     * Gson instance that is used to serialize and deserialize request and response bodies.
+     */
     private Gson gson;
+    /**
+     * Constant server API adress.
+     */
     private final static String API = "http://localhost:8080/api";
+    /**
+     * Constant JSON key to access server error messages.
+     */
     private final static String SERVER_ERROR_MSG_KEY = "error";
+    /**
+     * Singleton instance.
+     */
     private static HTTPService instance;
 
+    /**
+     * Helper method to hash Strings with SHA-256.
+     * Used for hashing passwords before sending them over network.
+     * However, works on any String instance.
+     * @param pw Password to be hashed with SHA-256
+     * @return SHA-256 Hash string of given password.
+     */
     private String getSHA256(String pw) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -45,11 +73,20 @@ public class HTTPService {
         return pw;
     }
 
+    /**
+     * Private constructor for HTTPService.
+     * Initializes restTemplate and gson
+     */
     private HTTPService() {
         this.restTemplate = new RestTemplate();
         this.gson = new Gson();
     }
 
+    /**
+     * Creates a new HTTPService instance and assigns it to instance field.
+     * Used for singleton access.
+     * @return instance
+     */
     public static HTTPService getInstance() {
         if (instance == null) {
             instance = new HTTPService();
@@ -58,6 +95,12 @@ public class HTTPService {
         return instance;
     }
 
+    /**
+     * Initializes a new HttpEntity and returns it.
+     * @param withToken Flag to add token header or not. Add if true, don't if false.
+     * @param json Json body to attach to created HttpEntity.
+     * @return newly created HttpEntity.
+     */
     private HttpEntity<String> initEntity(boolean withToken, String json) {
         HttpHeaders headers = new HttpHeaders();
         if (withToken) {
@@ -71,6 +114,15 @@ public class HTTPService {
         }
     }
 
+    /**
+     * Given a Player instance, logs in the user by sending a HTTP request to server
+     * and populates SessionStorage fields on successful login.
+     * Also logs if the operation was successful or not to console.
+     * Hashes the password with SHA-256 before sending it
+     * @param player Player to be logged in.
+     * @return True if login is successful, false otherwise.
+     * @see Player
+     */
     public boolean login(Player player) {
         player.setPassword(this.getSHA256(player.getPassword()));
         String json = gson.toJson(player);
@@ -89,6 +141,14 @@ public class HTTPService {
         return true;
     }
 
+    /**
+     * Registers the given player by sending HTTP request.
+     * Logs if the operation was successful or not to console.
+     * Hashes the password with SHA-256 before sending it
+     * @param player Player to be registered.
+     * @return Empty string on success, Server error message on error.
+     * @see Player
+     */
     public String register(Player player) {
         player.setPassword(this.getSHA256(player.getPassword()));
         String json = gson.toJson(player);
@@ -105,6 +165,13 @@ public class HTTPService {
         return UiInfoConstants.EMPTY_STRING;
     }
 
+    /**
+     * Sends a HTTP request to server to request a password reset code
+     * to given email address. Logs the success of operation to console.
+     * @param emailContainer Object that wraps email.
+     * @return Error message if operation failed, Empty string if operation was successful.
+     * @see PasswordReset
+     */
     public String sendCode(PasswordReset emailContainer) {
         String json = gson.toJson(emailContainer);
         HttpEntity<String> entity = initEntity(false, json);
@@ -123,6 +190,14 @@ public class HTTPService {
         return UiInfoConstants.EMPTY_STRING;
     }
 
+    /**
+     * Updates the password by sending a HTTP request to server.
+     * Logs the operation success status to console.
+     * Hashes the password with SHA-256 before sending it
+     * @param resetForm Form that holds reset code, username and new password.
+     * @return Empty string if operation was successful, error message otherwise.
+     * @see PasswordReset
+     */
     public String updatePassword(PasswordReset resetForm) {
         resetForm.setPassword(this.getSHA256(resetForm.getPassword()));
         String json = gson.toJson(resetForm);
@@ -144,10 +219,11 @@ public class HTTPService {
         return UiInfoConstants.EMPTY_STRING;
     }
 
-    public String getScoreboard(long days) {
-        return getScoreboard(days, 0);
-    }
-
+    /**
+     * Sends a get request to given URL with empty body and with token.
+     * @param url URL that the request is sent to.
+     * @return Server response.
+     */
     private String getRequest(String url) {
         HttpEntity<String> entity = initEntity(true, "");
         try {
@@ -160,15 +236,33 @@ public class HTTPService {
         }
     }
 
+    /**
+     * Gets the scoreboard for last n days with paging
+     * @param days Last n days
+     * @param page Requested page number.
+     * @return Server message.
+     */
     public String getScoreboard(long days, long page) {
         return this.getRequest(API + ServerFolders.SCOREBOARD_PATH + "/" + days + "?pageNumber=" + page);
     }
 
+    /**
+     * Starts a new game by sending a GET request to ServerFolders.NEW_GAME_PATH.
+     * Logs the username of the user who requested the new game to console.
+     * @return Server message
+     */
     public String startNewGame() {
         LOGGER.info("New Game request from " + SessionStorage.getInstance().getUsername());
         return this.getRequest(API + ServerFolders.NEW_GAME_PATH);
     }
 
+    /**
+     * POSTs score of the level to server.
+     * Logs username, game and score to console.
+     * @param level Level that score was calculated for.
+     * @return Server Response that contains player, game, level and score.
+     * @see PlayerGame
+     */
     public PlayerGame sendScores(Level level) {
         String json = gson.toJson(level);
         HttpEntity<String> entity = initEntity(true, json);
