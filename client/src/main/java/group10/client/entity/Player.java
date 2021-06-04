96537732cd139d2c5a00aa5e652b6bad51561bf7
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-06-04 16:54:23 +0300

    add player javadoc

--- client/src/main/java/group10/client/entity/Player.java
+++ client/src/main/java/group10/client/entity/Player.java
@@ -2,35 +2,82 @@ package group10.client.entity;
 
 import java.util.List;
 
+/**
+ * Player entity for parsing JSON responses from the server
+ *
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ */
 public class Player extends Base {
 
+    /**
+     * Field for storing username
+     */
     private String username;
+    /**
+     * Field for storing email
+     */
     private String email;
+    /**
+     * Field for storing roles of the user
+     */
     private List<Role> roles;
 
+    /**
+     * Default no parameter constructor
+     */
     public Player() {
     }
 
+    /**
+     * Gets username
+     *
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets username
+     *
+     * @param username new value of username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets email
+     *
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets email
+     *
+     * @param email new value of email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets roles
+     *
+     * @return roles
+     */
     public List<Role> getRoles() {
         return roles;
     }
 
+    /**
+     * Sets roles
+     *
+     * @param roles new roles list
+     */
     public void setRoles(List<Role> roles) {
         this.roles = roles;
     }
