9abf8465820c86df4f945cebd6a95f8e6ee8cbb0
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-06-04 15:21:00 +0300

    enums javadoc

--- client/src/main/java/group10/client/enums/MatchType.java
+++ client/src/main/java/group10/client/enums/MatchType.java
@@ -1,9 +1,26 @@
 package group10.client.enums;
 
+/**
+ *  @author Alperen Caykus, Mustafa Ozan Alpay
+ *  Enumarator for card matches.
+ *  Denotes what kind of match is present between two cards.
+ */
 public enum MatchType {
+    /**
+     * No match
+     */
     NO,
+    /**
+     * Regular match
+     */
     REGULAR,
+    /**
+     * Pisti match
+     */
     PISTI,
+    /**
+     * Double pisti match
+     */
     DOUBLE_PISTI;
 
 }
--- client/src/main/java/group10/client/enums/PlayerEnum.java
+++ client/src/main/java/group10/client/enums/PlayerEnum.java
@@ -1,12 +1,33 @@
 package group10.client.enums;
 
+/**
+ *  @author Alperen Caykus, Mustafa Ozan Alpay
+ *  Enumarator for player types.
+ *  Denotes the player's associated number.
+ */
 public enum PlayerEnum {
+    /**
+     * null player
+     */
     NULL(0),
+    /**
+     * first player, player itself
+     */
     ONE(1),
+    /**
+     * Second player, ai or human
+     */
     TWO(2);
 
+    /**
+     * Id of this
+     */
     private int id;
 
+    /**
+     * Constructor for this
+     * @param id id associated with enum.
+     */
     PlayerEnum(int id) {
         this.id = id;
     }
