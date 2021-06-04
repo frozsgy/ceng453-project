507b94c772a8589bc9fad5556c7477131bfc6a77
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-06-04 18:02:38 +0300

    register controller javadoc

--- client/src/main/java/group10/client/controller/RegisterController.java
+++ client/src/main/java/group10/client/controller/RegisterController.java
@@ -26,34 +26,80 @@ import java.util.ResourceBundle;
 import static group10.client.constants.UiConstants.LOGIN_FXML;
 import static group10.client.utility.UIUtility.centerScene;
 
+/**
+ * Controller class for Register screen.
+ * Implements FormView interface
+ * @see FormView
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ */
 public class RegisterController implements Initializable, FormView {
 
+    /**
+     * Username text input field
+     */
     @FXML
     private TextField username;
+    /**
+     * Password password input field
+     */
     @FXML
     private PasswordField password;
+    /**
+     * Email text input field
+     */
     @FXML
     private TextField email;
+    /**
+     * Button to navigate login page.
+     */
     @FXML
     private Button buttonBackRegister;
+    /**
+     * Button to submit register form.
+     */
     @FXML
     private Button buttonRegister;
+    /**
+     * Error message to display on unsuccessful register.
+     */
     @FXML
     private Text registerErrorMsg;
+    /**
+     * Middle anchor pane of screen.
+     */
     @FXML
     private AnchorPane registerMidPane;
+    /**
+     * Root border pane
+     */
     @FXML
     private BorderPane registerBorderPane;
+    /**
+     * Stackpane to attach loading spinner.
+     * @see LoadingSpinner
+     */
     @FXML
     private StackPane registerStackPane;
 
-
+    /**
+     * Initializes the scene
+     * @param url Address of this scene
+     * @param resourceBundle Resource bundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         buttonRegister.setDefaultButton(true);
         centerScene(this.registerBorderPane.getPrefWidth(), this.registerBorderPane.getPrefHeight());
     }
 
+    /**
+     * Callback method for buttonRegister.
+     * If the form is valid, submits form to server and starts spinner. Stops it when receives answer.
+     * If successful register, fires buttonBackRegister event and navigates back to login.
+     * If not successful, displays error message.
+     * @param event
+     * @see RegisterController#navigateToLogin(ActionEvent)
+     */
     @FXML
     protected void userRegister(ActionEvent event) {
         if (validateForm()) {
@@ -83,12 +129,19 @@ public class RegisterController implements Initializable, FormView {
         }
     }
 
+    /**
+     * Callback method for buttonBackRegister button. Navigates to login page.
+     * @param event Event caused by buttonBackRegister
+     */
     @FXML
     protected void navigateToLogin(ActionEvent event) {
         URL resource = getClass().getResource(LOGIN_FXML);
         Scene login = UIUtility.navigateTo(event, resource, null);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean validateForm() {
         if (username.getText().isEmpty() || password.getText().isEmpty() || email.getText().isEmpty()) {
@@ -96,18 +149,23 @@ public class RegisterController implements Initializable, FormView {
         }
         return true;
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setErrorMessage(String msg) {
         this.registerErrorMsg.setText(msg);
     }
 
-
+    /**
+     * Not implemented as on successful registration, navigates to login page.
+     */
     @Override
     public void setSuccessMessage(String msg) {
-
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void clearErrorMessage() {
         this.registerErrorMsg.setText(UiInfoConstants.EMPTY_STRING);
