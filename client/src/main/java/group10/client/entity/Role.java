e6b3334e37a917115461bded2b3fc2f9452720a4
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-06-04 16:52:31 +0300

    add javadoc for role entity

--- client/src/main/java/group10/client/entity/Role.java
+++ client/src/main/java/group10/client/entity/Role.java
@@ -1,16 +1,37 @@
 package group10.client.entity;
 
+/**
+ * Role entity for parsing JSON responses from the server
+ *
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ */
 public class Role extends Base {
 
+    /**
+     * Field for storing name of the role
+     */
     private String name;
 
+    /**
+     * Default no parameter constructor
+     */
     public Role() {
     }
 
+    /**
+     * Gets name
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name
+     *
+     * @param name new value of name
+     */
     public void setName(String name) {
         this.name = name;
     }
