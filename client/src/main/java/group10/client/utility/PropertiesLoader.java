9523c8c97a915fef30aef94dae4b9531ac43a00f
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-06-04 15:45:42 +0300

    add javadoc for properties loader

--- client/src/main/java/group10/client/utility/PropertiesLoader.java
+++ client/src/main/java/group10/client/utility/PropertiesLoader.java
@@ -7,47 +7,58 @@ import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 /**
- * TODO
- * JAVADOC
  * @author Alperen Caykus, Mustafa Ozan Alpay
  * Property loader class for UI constants.
- * */
+ */
 public class PropertiesLoader {
 
     /**
-     * Given a resource file name, loads it properties and returns it.
-     * @param resourceFileName Name of the resource file.
+     * Static PropertiesLoader instance.
+     */
+    private static PropertiesLoader instance;
+    /**
+     * Static Properties instance that holds properties.
+     */
+    private static Properties properties;
+
+    /**
+     * Loads properties from application.yml file and returns them.
+     *
      * @return Properties that were loaded from the file.
      * @throws IOException if input stream cannot be loaded to properties.
      */
-    private Properties loadProperties(String resourceFileName) throws IOException {
-
+    private Properties loadProperties() throws IOException {
         Properties properties = new Properties();
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(resourceFileName);
+        InputStream inputStream = classLoader.getResourceAsStream("application.yml");
         if (inputStream != null) {
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
             properties.load(inputStreamReader);
         }
         return properties;
-
     }
 
-
-    private static PropertiesLoader instance;
-    private static Properties properties;
-
+    /**
+     * Creates a singleton PropertiesLoader instance if not created before.
+     *
+     * @return PropertiesLoader instance.
+     */
     public static PropertiesLoader getInstance() {
         if (instance == null) {
             instance = new PropertiesLoader();
         }
         try {
-            properties = instance.loadProperties("application.yml");
+            properties = instance.loadProperties();
         } catch (IOException ignore) {
         }
         return instance;
     }
 
+    /**
+     * Creates a singleton Properties instance if not created before.
+     *
+     * @return Properties instance.
+     */
     public static Properties getProperties() {
         if (properties == null) {
             getInstance();
