c3bdaa55e4db167dca434a7041ceea503823ecd6
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-06-04 16:50:27 +0300

    add game and level entity javadoc

--- client/src/main/java/group10/client/entity/Game.java
+++ client/src/main/java/group10/client/entity/Game.java
@@ -2,18 +2,38 @@ package group10.client.entity;
 
 import group10.client.enums.GameTypes;
 
-
+/**
+ * Game entity for parsing JSON responses from the server
+ *
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ */
 public class Game extends Base {
 
+    /**
+     * GameType field
+     */
     private GameTypes gameType;
 
+    /**
+     * Default no parameter constructor
+     */
     public Game() {
     }
 
+    /**
+     * Gets game type
+     *
+     * @return game type
+     */
     public GameTypes getGameType() {
         return gameType;
     }
 
+    /**
+     * Sets game type
+     *
+     * @param gameType new game type value
+     */
     public void setGameType(GameTypes gameType) {
         this.gameType = gameType;
     }
--- client/src/main/java/group10/client/entity/Level.java
+++ client/src/main/java/group10/client/entity/Level.java
@@ -1,31 +1,68 @@
 package group10.client.entity;
 
+/**
+ * Level entity for parsing JSON responses from the server
+ *
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ */
 public class Level {
 
+    /**
+     * Field for storing game id
+     */
     private long game;
+
+    /**
+     * Field for storing score
+     */
     private long score;
 
-
+    /**
+     * Default no parameter constructor
+     */
     public Level() {
     }
 
+    /**
+     * Two parameters constructor
+     */
     public Level(long game, long score) {
         this.game = game;
         this.score = score;
     }
 
+    /**
+     * Gets game id
+     *
+     * @return game id
+     */
     public long getGame() {
         return game;
     }
 
+    /**
+     * Sets game id
+     *
+     * @param game new value of game
+     */
     public void setGame(long game) {
         this.game = game;
     }
 
+    /**
+     * Gets score
+     *
+     * @return score
+     */
     public long getScore() {
         return score;
     }
 
+    /**
+     * Sets score
+     *
+     * @param score new value of score
+     */
     public void setScore(long score) {
         this.score = score;
     }
