61dfcb2736a9483326a68a9037edfff049865556
Alperen Çaykuş <e2237170@ceng.metu.edu.tr>
2021-06-04 14:14:09 +0300

    javadoc for state

--- client/src/main/java/group10/client/state/ScoreboardStorage.java
+++ client/src/main/java/group10/client/state/ScoreboardStorage.java
@@ -3,12 +3,31 @@ package group10.client.state;
 import group10.client.entity.PagedEntity;
 import group10.client.entity.Scoreboard;
 
+/**
+ * @author Alperen Caykus, Mustafa Ozan Alpay
+ * Singleton storage class for scoreboard data.
+ */
 public class ScoreboardStorage {
 
+    /**
+     * Singleton instance.
+     */
     private static ScoreboardStorage instance;
+    /**
+     * Paged scoreboard data that is stored.
+     */
     private PagedEntity<Scoreboard> data;
+    /**
+     * Holds how many days are being displayed.
+     * For instance, if interval is 7, scores of last
+     * 7 days are stored.
+     */
     private int interval;
 
+    /**
+     * Creates an instance, assigns it to instance field and returns it.
+     * @return ScoreboardStorage instance.
+     */
     public static ScoreboardStorage getInstance() {
         if (instance == null) {
             instance = new ScoreboardStorage();
@@ -16,21 +35,40 @@ public class ScoreboardStorage {
         return instance;
     }
 
+    /**
+     * Private default constructor.
+     */
     private ScoreboardStorage() {
     }
 
+    /**
+     * Gets the data
+     * @return Paged scoreboard data.
+     */
     public PagedEntity<Scoreboard> getData() {
         return data;
     }
 
+    /**
+     * Sets the data
+     * @param data data to be set.
+     */
     public void setData(PagedEntity<Scoreboard> data) {
         this.data = data;
     }
 
+    /**
+     * Gets the interval
+     * @return interval
+     */
     public int getInterval() {
         return interval;
     }
 
+    /**
+     * Sets the interval
+     * @param interval interval to be set
+     */
     public void setInterval(int interval) {
         this.interval = interval;
     }
--- client/src/main/java/group10/client/state/SessionStorage.java
+++ client/src/main/java/group10/client/state/SessionStorage.java
@@ -1,14 +1,31 @@
 package group10.client.state;
 
 
+/**
+ *  @author Alperen Caykus, Mustafa Ozan Alpay
+ *  Singleton class to hold session information
+ *  of the logged in user.
+ */
 public class SessionStorage {
 
+    /**
+     * Username of the user.
+     */
     private String username;
+    /**
+     * Token that is returned from the server.
+     */
     private String token;
 
+    /**
+     * Singleton instance.
+     */
     private static SessionStorage instance;
 
-
+    /**
+     * Creates an instance, assigns it to instance field and returns it.
+     * @return SessionStorage instance.
+     */
     public static SessionStorage getInstance() {
         if (instance == null) {
             instance = new SessionStorage();
@@ -16,22 +33,42 @@ public class SessionStorage {
         return instance;
     }
 
+    /**
+     * Gets the username
+     * @return Username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the username
+     * @param username Username to be set.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets the token.
+     * @return Token
+     */
     public String getToken() {
         return token;
     }
 
+    /**
+     * Sets the token.
+     * @param token Token to be set.
+     */
     public void setToken(String token) {
         this.token = token;
     }
 
+    /**
+     * Logs the user out by setting
+     * token and username to null.
+     */
     public void logout() {
         this.token = null;
         this.username = null;
