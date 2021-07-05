af863871355306d4fb88b97db304f3c083837ccd
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-07-05 16:16:40 +0300

    solve blocking via delay

--- client/src/main/java/group10/client/controller/OpponentController.java
+++ client/src/main/java/group10/client/controller/OpponentController.java
@@ -52,14 +52,13 @@ public class OpponentController implements Runnable {
      * Handles opponent actions
      */
     public void play() {
-        /*
-        TODO uncomment this
+        
         try {
             Thread.sleep(ThreadLocalRandom.current().nextInt(125, 870));
         } catch (InterruptedException e) {
             LOGGER.error("User interrupt");
         }
-         */
+
         Platform.runLater(() -> {
             GameController._instance.playAsOpponent(updateBluff);
             GameController._instance.doTableActions();
