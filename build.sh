5e547fb830daf1106d7863ceee95b40606a4e8b5
Mustafa Ozan Alpay <frozsgy@gmail.com>
2021-07-05 22:05:05 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ rm -fr executables
 mkdir executables
 echo "Creating group10.war..."
 cd server
-mvn clean package
+mvn clean install package
 cd target
 cp group10.war ../../executables/
 echo "Creating group10.jar..."
 cd ../../client
-mvn clean package
+mvn clean install package
 cd target
 cp group10.jar ../../executables/
 
